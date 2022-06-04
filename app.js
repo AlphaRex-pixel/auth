5428461d01cb60719398fec8eb5ec62b89e9b6ee
AlphaRex-pixel <paulsa894@gmail.com>
2022-06-04 12:11:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,4 +61,4 @@ function verifyToken(req, res, next) {
 
 }
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+app.listen(process.env.PORT, () => console.log('Server started on port 5000'));
